bd4e3392aeb86ca92db9f701b55339ed8fed3951
hongmoSung <tesung30@naver.com>
2022-05-16 14:31:20 +0900

    feature: 스프링 배치 도메인 이해
    - JobInstance

--- src/main/java/io/springbatch/springbatchlecture/JobRunner.java
+++ src/main/java/io/springbatch/springbatchlecture/JobRunner.java
@@ -1,15 +1,16 @@
-package io.springbatch.springbatchlecture.config;
+package io.springbatch.springbatchlecture;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 @RequiredArgsConstructor
 public class JobRunner implements ApplicationRunner {
 
@@ -18,9 +19,8 @@ public class JobRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addString("name", "user2")
-                .toJobParameters();
+        JobParameters jobParameters =
+                new JobParametersBuilder().addString("name", "user2").toJobParameters();
         jobLauncher.run(job, jobParameters);
     }
 }
