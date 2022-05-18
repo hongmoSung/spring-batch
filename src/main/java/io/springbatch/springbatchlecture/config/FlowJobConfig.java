eb27432bb6b4b94cfc3a2b9fbbd1a5fa597e0416
hongmoSung <tesung30@naver.com>
2022-05-18 15:59:37 +0900

    feature: 스프링 배치 실행 - Flow
    - FlowJob - start() / next()

---
+++ src/main/java/io/springbatch/springbatchlecture/config/FlowJobConfig.java
@@ -0,0 +1,107 @@
+package io.springbatch.springbatchlecture.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class FlowJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job flowJob() {
+        return jobBuilderFactory.get("flowJob")
+                .start(flowA())
+                .next(step3())
+                .next(flowB())
+                .next(step6())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Flow flowA() {
+        FlowBuilder<Flow> flowFlowBuilder = new FlowBuilder<>("flowA");
+        return flowFlowBuilder.start(step1())
+                .next(step2())
+                .end();
+    }
+
+    private Flow flowB() {
+        FlowBuilder<Flow> flowFlowBuilder = new FlowBuilder<>("flowB");
+        return flowFlowBuilder.start(step4())
+                .next(step5())
+                .end();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step1 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step2 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step3 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step4 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step5 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step6() {
+        return stepBuilderFactory.get("step6")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step6 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
